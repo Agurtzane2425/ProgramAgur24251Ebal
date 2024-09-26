1fdd6b4aa23ae9e31d52cf7c9ecc2044da449719
Agurtzane Izagirre <agurtzaneii@fptxurdinaga.com>
2024-09-26 21:48:42 +0200

    Berriz aldatuko dut aurreko aldaketa Github-etik ezabatu dut

--- src/ebal1/agertuhamartarra2.java
+++ src/ebal1/agertuhamartarra2.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class agertuhamartarra2 {
 
 	public static void main(String[] args) {
-		System.out.print("Sartu Hamartar zenbaki bat Github: ");
+		System.out.print("Sartu Hamartar zenbaki bat Github aldaketa: ");
 		Scanner scanner = new Scanner(System.in);
 		double numero = scanner.nextDouble();
 		System.out.printf("zenbakia %.2f", numero);
