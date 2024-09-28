7f760bc5f671769fac17fa6b81d4ab3684c28eb4
Agurtzane Izagirre <agurtzaneii@fptxurdinaga.com>
2024-09-28 20:44:07 +0200

    WindowsBuilderOsagaiAtomikoen adibide bat

---
+++ src/ebal1/MyWindowBuilderExample.java
@@ -0,0 +1,65 @@
+package ebal1;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MyWindowBuilderExample {
+
+    public static void main(String[] args) {
+        // JFrame sortu
+        JFrame frame = new JFrame("WindowBuilder Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 400);
+        frame.setLayout(new FlowLayout());
+
+        // JLabel
+        JLabel label = new JLabel("Testu Etiketa");
+
+        // JButton
+        JButton button = new JButton("Sakatu");
+
+        // JCheckBox
+        JCheckBox checkBox = new JCheckBox("Aukera 1");
+
+        // JRadioButton
+        JRadioButton radioButton = new JRadioButton("Aukera 2");
+
+        // JToggleButton
+        JToggleButton toggleButton = new JToggleButton("Egoera Bikoitza");
+
+        // JComboBox
+        String[] items = {"Aukera A", "Aukera B", "Aukera C"};
+        JComboBox<String> comboBox = new JComboBox<>(items);
+
+        // JScrollBar
+        JScrollBar scrollBar = new JScrollBar();
+
+        // JSeparator
+        JSeparator separator = new JSeparator();
+
+        // JSlider
+        JSlider slider = new JSlider(0, 100);
+
+        // JSpinner
+        JSpinner spinner = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+
+        // JProgressBar
+        JProgressBar progressBar = new JProgressBar();
+
+        // Osagaiak gehitu
+        frame.add(label);
+        frame.add(button);
+        frame.add(checkBox);
+        frame.add(radioButton);
+        frame.add(toggleButton);
+        frame.add(comboBox);
+        frame.add(scrollBar);
+        frame.add(separator);
+        frame.add(slider);
+        frame.add(spinner);
+        frame.add(progressBar);
+
+        // Bistaratu
+        frame.setVisible(true);
+    }
+}
