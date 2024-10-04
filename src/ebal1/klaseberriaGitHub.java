e2e8a86e7808f25949107e514d7d59ef28071425
Agurtzane2425 <agurtzane.i@fptxurdinaga.com>
2024-10-04 13:19:32 +0200

    Klase berri bat sortu dugu lokalean ikastetxekoAdarrean

---
+++ src/ebal1/klaseberriaGitHub.java
@@ -0,0 +1,17 @@
+package ebal1;
+
+import java.util.Scanner;
+
+public class klaseberriaGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sartu zenbaki oso bat: ");
+		Scanner scanner = new Scanner(System.in);
+		int numero = scanner.nextInt();
+		System.out.printf("Zenbaki osoa %d da", numero);
+		scanner.close();
+
+	}
+
+}
